055707221ddbc0226e19152d41ffd3f5a3022ad1
luhouyang <luhouyang@gmail.com>
2024-06-09 04:19:28 +0800

    storage code done

--- model/cloud_storage.py
+++ model/cloud_storage.py
@@ -12,21 +12,26 @@ def upload_blob_from_memory(contents, destination_blob_name, bucket_name="imagin
     full_destination_name = st.session_state.user_info['id'] + '/' + destination_blob_name
     blob = bucket.blob(full_destination_name)
 
-    byte_data = (" ".join(contents)).encode('utf-8')
+    byte_data = ("".join(contents)).encode('utf-8')
     blob.upload_from_string(byte_data)
 
-    newBlobs = st.session_state.userdata['blobs_urls'].append(full_destination_name)
-    userdata = UserdataEntity(**st.session_state.userdata, blobs_urls=newBlobs)
-    set_userdata(userdata)
+    st.session_state.userdata['blobs_urls'].append(full_destination_name)
+    userdata = UserdataEntity(**st.session_state.userdata)
+    set_userdata(userdata.to_dict())
 
 
-def get_blob_from_memory(bucket_name="imagine-whack.appspot.com"):
-    storage_client = storage.Client.from_service_account_info(st.secrets['firebase_key'])
+def get_blob_from_firebase(bucket_name="imagine-whack.appspot.com"):
+    storage_client = storage.Client.from_service_account_info(st.secrets['firebases_key'])
     bucket = storage_client.bucket(bucket_name)
 
     uris = st.session_state.userdata['blobs_urls']
     data = []
     for uri in uris:
-        blob = bucket.get_blob(uri)
-        data.append(str(blob.decode('utf-8')))
+        blob = bucket.blob(uri)
+        print(blob.name.split("/")[1])
+        res = blob.download_as_bytes()
+        data.append({
+            'name': blob.name.split("/")[1],
+            'content': str(res.decode('utf-8'))
+            })
     return data
