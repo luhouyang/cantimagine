0b2a0197f88c5c73a16db6e0491adb2843435320
CJ Seven <hperm9166@gmail.com>
2024-06-09 00:46:25 +0800

    changed firebase-key

--- model/cloud_storage.py
+++ model/cloud_storage.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from google.cloud import storage
 
+
 def upload_blob_from_memory(contents, destination_blob_name, bucket_name="imagine-whack.appspot.com"):
-    storage_client = storage.Client.from_service_account_info(st.secrets['firebase'])
+    storage_client = storage.Client.from_service_account_info(
+        st.secrets['firebases_key'])
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(destination_blob_name)
 
--- model/firestore_model.py
+++ model/firestore_model.py
@@ -1,8 +1,10 @@
 import streamlit as st
 from google.cloud import firestore
 
+
 def set_userdata(output):
-    db = firestore.Client.from_service_account_info(st.secrets['firebase'])
+    db = firestore.Client.from_service_account_info(
+        st.secrets['firebases_key'])
     doc_ref = db.collection("users").document().id
     db.collection("users").document(doc_ref).set(
         {
@@ -11,8 +13,10 @@ def set_userdata(output):
         }
     )
 
+
 def get_userdata():
     # Then query to list all users
-    db = firestore.Client.from_service_account_info(st.secrets['firebase'])
+    db = firestore.Client.from_service_account_info(
+        st.secrets['firebases_key'])
     users_ref = db.collection('users')
     return users_ref
